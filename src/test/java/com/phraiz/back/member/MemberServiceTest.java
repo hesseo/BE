bafd7d09030bb75fb678db7d39ce95016a572226
hesseo <heeseo3670@naver.com>
2025-07-17 17:31:26 +0900

    chore: 불필요한 코드 주석

--- src/test/java/com/phraiz/back/member/MemberServiceTest.java
+++ src/test/java/com/phraiz/back/member/MemberServiceTest.java
@@ -1,38 +1,38 @@
-package com.phraiz.back.member;
-
-import com.phraiz.back.member.domain.Member;
-import com.phraiz.back.member.dto.request.SignUpRequestDTO;
-import com.phraiz.back.member.enums.LoginType;
-import com.phraiz.back.member.repository.MemberRepository;
-import com.phraiz.back.member.service.MemberService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@Transactional
-public class MemberServiceTest {
-
-    @Autowired
-    private MemberService memberService;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Test
-    public void signUp() {
-        // given
-        SignUpRequestDTO dto = new SignUpRequestDTO();
-        dto.setId("hee");
-        dto.setPwd("1234");
-        dto.setEmail("hee@gmail.com");
-        dto.setLoginType(LoginType.LOCAL);
-        // when
-        memberService.signUp(dto);
-        // then
-        Member saved = memberRepository.findById("hee").orElse(null);
-        assert saved != null;
-        assert saved.getEmail().equals("hee@gmail.com");
-    }
-}
+//package com.phraiz.back.member;
+//
+//import com.phraiz.back.member.domain.Member;
+//import com.phraiz.back.member.dto.request.SignUpRequestDTO;
+//import com.phraiz.back.member.enums.LoginType;
+//import com.phraiz.back.member.repository.MemberRepository;
+//import com.phraiz.back.member.service.MemberService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//public class MemberServiceTest {
+//
+//    @Autowired
+//    private MemberService memberService;
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @Test
+//    public void signUp() {
+//        // given
+//        SignUpRequestDTO dto = new SignUpRequestDTO();
+//        dto.setId("hee");
+//        dto.setPwd("1234");
+//        dto.setEmail("hee@gmail.com");
+//        dto.setLoginType(LoginType.LOCAL);
+//        // when
+//        memberService.signUp(dto);
+//        // then
+//        Member saved = memberRepository.findById("hee").orElse(null);
+//        assert saved != null;
+//        assert saved.getEmail().equals("hee@gmail.com");
+//    }
+//}
